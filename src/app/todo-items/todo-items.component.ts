16a57198af364491e9d1e430bdedcfbd98bf86c2
wellwind <wellwind@mail2000.com.tw>
2016-11-20 10:49:35 +0800

    using @Input to receive data

--- src/app/todo-items/todo-items.component.ts
+++ src/app/todo-items/todo-items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './../shared/todo-item';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-items',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-items.component.css']
 })
 export class TodoItemsComponent implements OnInit {
+
+  @Input() items: TodoItem[];
 
   constructor() { }
 
