d1c1cdba62f149ee619d5c31aab4d48c03b9fe60
wellwind <wellwind@mail2000.com.tw>
2016-11-20 10:36:04 +0800

    Add fake data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './shared/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  todoItems: TodoItem[] = [{
+    id: 1,
+    value: 'Todo Item No.1',
+    done: false
+  }, {
+    id: 2,
+    value: 'Todo Item No.2',
+    done: true
+  }, {
+    id: 3,
+    value: 'Todo Item No.3',
+    done: false
+  }];
+
 }
