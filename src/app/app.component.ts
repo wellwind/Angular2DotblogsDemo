9bc07c7cf365cddbba30be9a7841f855592efae1
wellwind <wellwind@mail2000.com.tw>
2017-02-03 21:40:48 +0800

    Complete TodoListService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TodoListService } from './todo-list.service';
 import { Component } from '@angular/core';
 import { TodoItem } from './shared/todo-item';
 
@@ -8,32 +9,20 @@ import { TodoItem } from './shared/todo-item';
 })
 export class AppComponent {
   title = 'app works!';
-  todoItems: TodoItem[] = [{
-    id: 1,
-    value: 'Todo Item No.1',
-    done: false
-  }, {
-    id: 2,
-    value: 'Todo Item No.2',
-    done: true
-  }, {
-    id: 3,
-    value: 'Todo Item No.3',
-    done: false
-  }];
+
+  constructor(private todoListService: TodoListService) {
+  }
+
+  getTodoList() {
+    return this.todoListService.getTodoList;
+  }
 
   addTodo(text) {
-    this.todoItems.push({
-      id: (new Date()).getTime(),
-      value: text,
-      done: false
-    });
+
   }
 
   deleteItem(item: TodoItem) {
-    console.log(item);
-    this.todoItems = this.todoItems.filter(todoItem => todoItem.id !== item.id);
-    console.log(this.todoItems);
+
   }
 
 }
