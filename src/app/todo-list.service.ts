221594340bd00c494ff3491b1042027ed44092da
wellwind <wellwind@mail2000.com.tw>
2017-02-04 16:24:55 +0800

    Use Http to load todo list

--- src/app/todo-list.service.ts
+++ src/app/todo-list.service.ts
@@ -1,24 +1,23 @@
+import { Http } from '@angular/http';
 import { TodoItem } from './shared/todo-item';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class TodoListService {
 
-  todoItems: TodoItem[] = [{
-    id: 1,
-    value: 'Todo Item No.1',
-    done: false
-  }, {
-    id: 2,
-    value: 'Todo Item No.2',
-    done: true
-  }, {
-    id: 3,
-    value: 'Todo Item No.3',
-    done: false
-  }];
+  todoItems: TodoItem[];
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  loadTodoList() {
+    this.http
+      .get('/assets/todo-list.json')
+      .toPromise()
+      .then(response => {
+        this.todoItems = response.json();
+      });
+  }
 
   getTodoList() {
     return this.todoItems;
