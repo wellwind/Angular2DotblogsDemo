d6738a932ea45dec1d7622d0783f3d5214671dbd
wellwind <wellwind@mail2000.com.tw>
2017-02-06 21:38:35 +0800

    DatePipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-
+  today = new Date();
   constructor(private todoListService: TodoListService) {
   }
 }
