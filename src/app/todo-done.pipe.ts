0376a7b9a2e9846b4f2e4a3ca4d151b12aa90988
wellwind <wellwind@mail2000.com.tw>
2017-02-12 21:20:54 +0800

    Impure Pipe

--- src/app/todo-done.pipe.ts
+++ src/app/todo-done.pipe.ts
@@ -2,7 +2,8 @@ import { TodoItem } from './shared/todo-item';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'todoDone'
+  name: 'todoDone',
+  pure: false
 })
 export class TodoDonePipe implements PipeTransform {
 
