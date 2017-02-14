b50ac7f42cc6f79daeb28b55d344bdba655930c2
wellwind <wellwind@mail2000.com.tw>
2017-02-14 21:54:45 +0800

    Use @HostListener

--- src/app/bs-button.directive.ts
+++ src/app/bs-button.directive.ts
@@ -1,4 +1,4 @@
-import { Renderer, ElementRef, Directive, OnInit, Input } from '@angular/core';
+import { HostListener, Renderer, ElementRef, Directive, OnInit, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBsButton]'
@@ -6,11 +6,28 @@ import { Renderer, ElementRef, Directive, OnInit, Input } from '@angular/core';
 export class BsButtonDirective implements OnInit {
 
   @Input() appBsButton;
+  @Input() mouseDownClass; // 額外的@Input
 
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
+    this.appBsButton = this.appBsButton || 'primary';
+    this.mouseDownClass = this.mouseDownClass || 'danger';
     this.renderer.setElementClass(this.el.nativeElement, 'btn', true);
-    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.appBsButton || 'primary'}`, true);
+    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.appBsButton}`, true);
+  }
+
+  @HostListener('mousedown') onMouseDown() {
+    // 移除原來的樣式
+    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.appBsButton}`, false);
+    // 加入mousedown的樣式
+    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.mouseDownClass}`, true);
+  }
+
+  @HostListener('mouseup') onMouseUp() {
+    // 移除mousedown的樣式
+    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.mouseDownClass}`, false);
+    // 加入原來的樣式
+    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.appBsButton}`, true);
   }
 }
