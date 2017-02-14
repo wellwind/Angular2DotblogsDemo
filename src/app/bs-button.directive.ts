a97cdf4407e7d9e1d3d89b8074e8bb46b1419b69
wellwind <wellwind@mail2000.com.tw>
2017-02-14 21:38:39 +0800

    Use @Input

--- src/app/bs-button.directive.ts
+++ src/app/bs-button.directive.ts
@@ -1,14 +1,16 @@
-import {Renderer, ElementRef,  Directive,  OnInit} from '@angular/core';
+import { Renderer, ElementRef, Directive, OnInit, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBsButton]'
 })
 export class BsButtonDirective implements OnInit {
 
+  @Input() appBsButton;
+
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
     this.renderer.setElementClass(this.el.nativeElement, 'btn', true);
-    this.renderer.setElementClass(this.el.nativeElement, 'btn-primary', true);
+    this.renderer.setElementClass(this.el.nativeElement, `btn-${this.appBsButton || 'primary'}`, true);
   }
 }
