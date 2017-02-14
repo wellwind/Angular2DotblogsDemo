3abb1c75b03bf1d8a1c900d5051d82ac65efe34f
wellwind <wellwind@mail2000.com.tw>
2017-02-14 20:24:53 +0800

    Geneerate directive

---
+++ src/app/bs-button.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BsButtonDirective } from './bs-button.directive';
+
+describe('BsButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BsButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/bs-button.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appBsButton]'
+})
+export class BsButtonDirective {
+
+  constructor() { }
+
+}
